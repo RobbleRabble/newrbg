6d443dfb388c6f079b526e149ff17733186c5f88
Rob Kilduff <rkilduff88@gmail.com>
2020-05-08 11:05:06 -0400

    values track

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -15,7 +15,11 @@ const Wheel = (props) => {
     setHue(h)
     setSaturation(s)
     setLightness(l)
+    // useEffect()
   }
+  // const useEffect = () => {
+  //   setLightness()
+  // }
 
   const trackHueChanges = (e) => {
     const h = e.target.value
@@ -41,25 +45,32 @@ const Wheel = (props) => {
           <input
             onChange={trackHueChanges}
             type="range"
+            value={hue}
             min="0"
             max="360"
             step="1"
             id="hue"
           ></input>
+        </div>
+        <div className="slider">
           Saturation
           <input
             onChange={trackSatChanges}
             type="range"
+            value={saturation}
             min="1"
             max="100"
             step="1"
             id="sat"
             // onClick={this.changeSat}
           ></input>
+        </div>
+        <div className="slider">
           Lightness
           <input
             onChange={trackLightChanges}
             type="range"
+            value={lightness}
             min="1"
             max="100"
             step="1"
@@ -67,11 +78,12 @@ const Wheel = (props) => {
             // onClick={this.changeLight}
           ></input>
           <div
+            className="selected"
             style={{
               backgroundColor: `hsl(${hue},${saturation}%,${lightness}%)`,
             }}
           >
-            <h2 class="Display=Text">
+            <h2 className="Display-Text">
               hsl({hue},{saturation}%,
               {lightness}%)
             </h2>
@@ -81,7 +93,7 @@ const Wheel = (props) => {
             }}
             </div> */}
         </div>
-        <button class="try-random" onClick={random}>
+        <button className="random" onClick={random}>
           Try a random!
         </button>
       </section>
@@ -90,104 +102,3 @@ const Wheel = (props) => {
 }
 
 export default Wheel
-
-// import React, { Component } from 'react'
-// import axios from 'axios'
-
-// export class Wheel extends Component {
-//   state = {
-//     displayHex: '',
-//     selectedColor: this.props.initialColor,
-//     colorHue: this.props.initialDisplayedHue,
-//     hueInputBox: '',
-//     colorSaturation: this.props.initialDisplayedSat,
-//     satInputBox: '',
-//     colorLightness: this.props.light,
-//     lightInputBox: '',
-//   }
-
-//   trackHueChange = (e) => {
-//     console.log(e.target.value)
-//     this.setState({
-//       hueInputBox: e.target.value,
-//     })
-//   }
-
-//   trackSatChange = (e) => {
-//     console.log(e.target.value)
-//     this.setState({
-//       satInputBox: e.target.value,
-//     })
-//   }
-
-//   trackLightChange = (e) => {
-//     console.log(e.target.value)
-//     this.setState({
-//       lightInputBox: e.target.value,
-//     })
-//   }
-
-//   handleChangeComplete = (color) => {
-//     this.setState({ background: color.hex })
-//   }
-
-//   render() {
-//     console.log('rendering page')
-//     return (
-//       <>
-//         <section class="container">
-//           <div class="slider">
-//             Hue
-//             <input
-//               onChange={this.trackHueChange}
-//               type="range"
-//               min="0"
-//               max="360"
-//               step="1"
-//               id="hue"
-//             ></input>
-//             Saturation
-//             <input
-//               onChange={this.trackSatChange}
-//               type="range"
-//               min="1"
-//               max="100"
-//               step="1"
-//               id="sat"
-//               // onClick={this.changeSat}
-//             ></input>
-//             Lightness
-//             <input
-//               onChange={this.trackLightChange}
-//               type="range"
-//               min="1"
-//               max="100"
-//               step="1"
-//               id="light"
-//               // onClick={this.changeLight}
-//             ></input>
-//             <div
-//               style={{
-//                 backgroundColor: `hsl(${this.state.hueInputBox},${this.state.satInputBox}%,${this.state.lightInputBox}%)`,
-//               }}
-//             >
-//               <h2 class="Display=Text">
-//                 hsl({this.state.hueInputBox},{this.state.satInputBox}%,
-//                 {this.state.lightInputBox}%)
-//               </h2>
-//             </div>
-//             {/* <div style={{
-//               text: 'my text',
-//             }}
-//             </div> */}
-//           </div>
-//           {/* <input>Try a random!</input> */}
-//         </section>
-//       </>
-//     )
-//   }
-// }
-
-// document.addEventListener('DOMContentLoaded', Wheel)
-
-// export default Wheel
